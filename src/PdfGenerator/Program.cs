6e52bb778f76c1e3b3f29f96807bffc9a63d3d8a
Bebo-Maker <bennetbo@gmx.de>
2022-05-24 22:02:11 +0200

    Implement first prototype.

--- src/PdfGenerator/Program.cs
+++ src/PdfGenerator/Program.cs
@@ -1,3 +1,37 @@
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+
+static IDocument CreateDocument(PdfGenerationData data)
+{
+  return Document.Create(c =>
+  {
+    c.Page(p =>
+    {
+      p.Margin(50);
+      p.Size(data.Width, data.Height, Unit.Point);
+
+      p.Content()
+       .DefaultTextStyle(t => t.FontSize(30))
+       .Column(c =>
+       {
+         foreach (var i in Enumerable.Range(0, data.PageCount))
+         {
+           c.Item().Text("Hallo :)");
+           if (i < data.PageCount - 1)
+             c.Item().PageBreak();
+         }
+       });
+      p.Footer().AlignCenter().Text(t =>
+      {
+        t.CurrentPageNumber();
+        t.Span(" / ");
+        t.TotalPages();
+      });
+    });
+  });
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,4 +50,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/generate/{width}/{height}/{pages}", (int width, int height, int pages) =>
+{
+  var document = CreateDocument(new(width, height, pages));
+  var pdfData = document.GeneratePdf();
+  return Results.Bytes(pdfData, contentType: "application/pdf");
+});
+
 app.Run();
+
+public record PdfGenerationData(int Width, int Height, int PageCount);
