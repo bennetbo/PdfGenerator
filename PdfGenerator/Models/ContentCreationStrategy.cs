569aac2f3d33b60c27285a798a647c82eb93f16f
Squawnchy <marc-gilbrecht@outlook.de>
2022-06-06 20:55:33 +0200

    Moved content creation strats to models dir

--- PdfGenerator/Models/ContentCreationStrategy.cs
+++ PdfGenerator/Models/ContentCreationStrategy.cs
@@ -1,4 +1,5 @@
-﻿using PdfGenerator.DTOs;
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 namespace PdfGenerator.Models;
@@ -10,5 +11,32 @@ public interface IPdfContentService<PdfContentType>
 {
   IContentCreationStrategy GetContentCreationStrategy(PdfContentType pageContent, int width, int height);
 }
-
-
+public record EmptyContentCreationStrategy() : IContentCreationStrategy
+{
+  public void Use(IContainer container) { }
+}
+public record CustomContentCreationStrategy(string Content) : IContentCreationStrategy
+{
+  public void Use(IContainer container) => container.Text(Content);
+}
+public record RandomContentCreationStrategy() : IContentCreationStrategy
+{
+  public void Use(IContainer container) => container.Text(Placeholders.Sentence());
+}
+public record CatImageContentCreationStrategy() : IContentCreationStrategy
+{
+  public void Use(IContainer container) => container.Image("./Images/Professor.jpeg");
+}
+public record ImageContentCreationStrategy(int Width, int Height) : IContentCreationStrategy
+{
+  public void Use(IContainer container) => container.Image(Placeholders.Image(Width, Height));
+}
+public record PageCounterContentStrategy() : IContentCreationStrategy
+{
+  public void Use(IContainer container) => container.AlignCenter().Text(t =>
+  {
+    t.CurrentPageNumber();
+    t.Span(" / ");
+    t.TotalPages();
+  });
+}
--- PdfGenerator/Services/PageContentService.cs
+++ PdfGenerator/Services/PageContentService.cs
@@ -1,39 +1,18 @@
 ﻿using PdfGenerator.DTOs;
 using PdfGenerator.Models;
-using QuestPDF.Fluent;
-using QuestPDF.Helpers;
-using QuestPDF.Infrastructure;
-
 namespace PdfGenerator.Services;
 public class PageContentService : IPdfContentService<PdfPageContent>
 {
-  record EmptyContentCreationStrategy() : IContentCreationStrategy
-  {
-    public void Use(IContainer container) { }
-  }
-  record CustomContentCreationStrategy(string Content) : IContentCreationStrategy
-  {
-    public void Use(IContainer container) => container.Text(Content);
-  }
-  record RandomContentCreationStrategy() : IContentCreationStrategy
-  {
-    public void Use(IContainer container) => container.Text(Placeholders.Sentence());
-  }
-  record CatImageContentCreationStrategy() : IContentCreationStrategy
-  {
-    public void Use(IContainer container) => container.Image("./Images/Professor.jpeg");
-  }
-  record ImageContentCreationStrategy(int Width, int Height) : IContentCreationStrategy
-  {
-    public void Use(IContainer container) => container.Image(Placeholders.Image(Width, Height));
-  }
-
-
-  public IContentCreationStrategy CreateEmtpyContentStrategy() => new EmptyContentCreationStrategy();
-  public IContentCreationStrategy CreateCustomTextContentStrategy(int pageIndex, params string[] Contents) => new CustomContentCreationStrategy(Contents[pageIndex]);
-  public IContentCreationStrategy CreateCatImageContentStrategy() => new CatImageContentCreationStrategy();
-  public IContentCreationStrategy CreateImageContentStrategy(int width, int height) => new ImageContentCreationStrategy(width, height);
-  public IContentCreationStrategy CreateRandomTextContentStrategy() => new RandomContentCreationStrategy();
+  public IContentCreationStrategy CreateEmtpyContentStrategy() 
+    => new EmptyContentCreationStrategy();
+  public IContentCreationStrategy CreateCustomTextContentStrategy(int pageIndex, params string[] Contents) 
+    => new CustomContentCreationStrategy(Contents[pageIndex]);
+  public IContentCreationStrategy CreateCatImageContentStrategy() 
+    => new CatImageContentCreationStrategy();
+  public IContentCreationStrategy CreateImageContentStrategy(int width, int height) 
+    => new ImageContentCreationStrategy(width, height);
+  public IContentCreationStrategy CreateRandomTextContentStrategy() 
+    => new RandomContentCreationStrategy();
 
   public IContentCreationStrategy GetContentCreationStrategy(PdfPageContent pageContent, int width, int height) => pageContent switch
   {
