b83105618cebb225ead0c4b95a5d0fe3e3091be9
Bebo-Maker <bennetbo@gmx.de>
2022-06-08 20:34:31 +0200

    Remove change directory from workflow.

--- PdfGenerator/Program.cs
+++ PdfGenerator/Program.cs
@@ -6,7 +6,6 @@ using PdfGenerator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer()
