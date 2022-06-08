e7a5803a2eb0c5730fe6ffc8d007c83cfce7fe60
Bebo-Maker <bennetbo@gmx.de>
2022-06-08 20:30:46 +0200

    Test change file.

--- PdfGenerator/Program.cs
+++ PdfGenerator/Program.cs
@@ -6,6 +6,7 @@ using PdfGenerator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer()
