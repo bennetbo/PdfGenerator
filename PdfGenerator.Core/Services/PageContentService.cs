4a8e1dfa616871466fd7440ef8c35d095be283ac
Bebo-Maker <bennetbo@gmx.de>
2022-06-29 20:31:23 +0200

    Move professor image to resources.

--- PdfGenerator.Core/Services/PageContentService.cs
+++ PdfGenerator.Core/Services/PageContentService.cs
@@ -1,5 +1,6 @@
 ﻿using PdfGenerator.Core.Enums;
 using PdfGenerator.Core.Models;
+using PdfGenerator.Core.Properties;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using System;
@@ -13,7 +14,7 @@ public class PageContentService : IPdfContentService<PdfPageContent>
     PdfPageContent.RandomSentences => new ContentCreationStrategy(c => c.Text(Placeholders.Sentence())),
     PdfPageContent.Empty => new ContentCreationStrategy(_c => { }),
     PdfPageContent.Images => new ContentCreationStrategy(c => c.Image(Placeholders.Image(width, height))),
-    PdfPageContent.CatImages => new ContentCreationStrategy(c => c.Image("./Images/Professor.jpeg")),
+    PdfPageContent.CatImages => new ContentCreationStrategy(c => c.Image(Resources.Professor)),
     _ => throw new NotImplementedException(),
   };
 }
