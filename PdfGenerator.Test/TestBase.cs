8fdafd13525936a65d6e930b188423290facdec3
Bennet Bo Fenner <53836821+Bebo-Maker@users.noreply.github.com>
2022-08-21 23:35:49 +0200

    Add base class for tests.

---
+++ PdfGenerator.Test/TestBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace PdfGenerator.Test;
+
+public abstract class TestBase<T>
+{
+  [NotNull] //Marking as not null here, because setup method is called before each test run.
+  protected T? Sut { get; private set; }
+
+  [SetUp]
+  public void Setup()
+  {
+    Sut = DoSetup();
+  }
+
+  [TearDown]
+  public void TearDown()
+  {
+    Sut = default;
+  }
+
+  protected abstract T DoSetup();
+}
